eece950d93eab59b87833bf9849cffe785bb1450
Derrick Ibeto <derrickibeto@Derricks-MacBook-Pro.local>
2023-10-24 14:19:51 +0100

    Latest changestone to milestone_2

---
+++ hangman/milestone_2.py
@@ -0,0 +1,20 @@
+import random
+
+word_list = ['grapes','apple','apple','pineapple','banana']
+
+print(word_list)
+
+def random_choice(word_list):
+    word = random.choice(word_list)
+    print(word)
+
+
+random_choice(word_list)
+
+guess = input("Enter a single letter")
+
+if len(guess) == 1 & guess.isalpha():
+    print("Good guess!")
+
+else:
+    print("Oops! That is not a valid input.")
